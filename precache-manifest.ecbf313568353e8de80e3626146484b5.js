b2b7d89f794fa020f0dd2cb0980825ba184a8466
Colin Cummings <colinrcummings@Colins-MacBook-Pro.local>
2020-03-30 22:44:59 -0400

    Updates

---
+++ precache-manifest.ecbf313568353e8de80e3626146484b5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3ec52461e895529a20c2b4690d985f1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9994a823ed9f67ea9e11",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "132ac8bd5dfcb93652be",
+    "url": "/static/css/main.7c1a2188.chunk.css"
+  },
+  {
+    "revision": "9994a823ed9f67ea9e11",
+    "url": "/static/js/2.25677304.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.25677304.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "132ac8bd5dfcb93652be",
+    "url": "/static/js/main.37050420.chunk.js"
+  },
+  {
+    "revision": "5f3c49ebe5d2911bba96",
+    "url": "/static/js/runtime-main.38b73d22.js"
+  }
+]);
