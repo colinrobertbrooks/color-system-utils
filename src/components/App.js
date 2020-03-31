bff3fccd0a4664ed4ca96966c153b0f5f4983629
Colin Cummings <colinrcummings@Colins-MacBook-Pro.local>
2020-03-30 22:38:23 -0400

    show delta e

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,7 @@ import ItemInputGroup from "./ItemInputGroup";
 import ItemsList from "./ItemsList";
 import ItemSorts from "./ItemSorts";
 import useItems from "../hooks/useItems";
+import calcDeltaE from "../utils/calcDeltaE";
 
 function App() {
   const [palette1Items, setPalette1Items] = useItems({
@@ -14,6 +15,7 @@ function App() {
     localStorageKey: "PALETTE_2_ITEMS"
   });
   const [showInverses, setShowInverses] = useState(false);
+  const [showDeltaE, setShowDeltaE] = useState(false);
 
   return (
     <Container>
@@ -25,17 +27,27 @@ function App() {
         <Col xs={3} />
         <Col xs={3} className="text-center mb-3">
           <Checkbox
-            label="Show inverses"
+            label="Show Inverses"
             checked={showInverses}
             onChange={event => setShowInverses(event.target.checked)}
           />
         </Col>
         <Col xs={3} className="text-center mb-3">
-          <p>TODO</p>
+          <Checkbox
+            label="Show Delta E"
+            checked={showDeltaE}
+            onChange={event => setShowDeltaE(event.target.checked)}
+          />
         </Col>
         <Col xs={3} />
-        <Col xs={6}>
+        <Col xs={5}>
           <h2>Palette 1</h2>
+        </Col>
+        <Col xs={2} />
+        <Col xs={5}>
+          <h2>Palette 1</h2>
+        </Col>
+        <Col xs={5}>
           <ItemsList
             items={palette1Items}
             setItems={setPalette1Items}
@@ -48,8 +60,45 @@ function App() {
             <ItemInputGroup items={palette1Items} setItems={setPalette1Items} />
           </div>
         </Col>
-        <Col xs={6}>
-          <h2>Palette 2</h2>
+        <Col xs={2}>
+          {showDeltaE && (
+            <div
+              style={{
+                border: "1px solid transparent",
+                display: "flex",
+                flexDirection: "column",
+                padding: 16
+              }}
+            >
+              {palette1Items.map((_, idx) => {
+                const hex1 = palette1Items[idx];
+                const hex2 = palette2Items[idx];
+
+                if (hex1 && hex2) {
+                  return (
+                    <div
+                      key={idx}
+                      title={`Delta E of ${hex1} and ${hex2}`}
+                      style={{
+                        border: "1px solid transparent",
+                        fontStyle: "italic",
+                        marginTop: showInverses ? 27 : 0,
+                        marginBottom: 8,
+                        padding: "16px 0",
+                        textAlign: "center"
+                      }}
+                    >
+                      {parseFloat(calcDeltaE(hex1, hex2)).toFixed(1)}%
+                    </div>
+                  );
+                }
+
+                return null;
+              })}
+            </div>
+          )}
+        </Col>
+        <Col xs={5}>
           <ItemsList
             items={palette2Items}
             setItems={setPalette2Items}
