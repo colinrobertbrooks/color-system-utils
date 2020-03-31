699dbf8886a526f6f867ff9164877fe7ed1f1a88
Colin Cummings <colinrcummings@Colins-MacBook-Pro.local>
2020-03-30 22:46:01 -0400

    Updates

---
+++ precache-manifest.622eecdcbf6343a017aa47b04c349206.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5125b36ee154703a67e7b75740e75a95",
+    "url": "/color-system-utils/index.html"
+  },
+  {
+    "revision": "9994a823ed9f67ea9e11",
+    "url": "/color-system-utils/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "132ac8bd5dfcb93652be",
+    "url": "/color-system-utils/static/css/main.7c1a2188.chunk.css"
+  },
+  {
+    "revision": "9994a823ed9f67ea9e11",
+    "url": "/color-system-utils/static/js/2.25677304.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/color-system-utils/static/js/2.25677304.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "132ac8bd5dfcb93652be",
+    "url": "/color-system-utils/static/js/main.37050420.chunk.js"
+  },
+  {
+    "revision": "1053a3c6518511857696",
+    "url": "/color-system-utils/static/js/runtime-main.e9044921.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ecbf313568353e8de80e3626146484b5.js"
+  "/color-system-utils/precache-manifest.622eecdcbf6343a017aa47b04c349206.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/color-system-utils/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
